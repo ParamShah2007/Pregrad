defed56f8815448fb7682e5edbdd1f841b1b3a31
ParamShah2007 <param100cs2024@gmail.com>
2026-02-13 01:01:27 +0530

    E-Commerce

--- Assignment_4/script.js
+++ Assignment_4/script.js
@@ -1,2 +1,23 @@
 import products from "./data";
-console.log(products)
+// console.log(products)
+const productsDisplay = document.getElementById('productsDisplay');
+const displayProducts = products=>{
+    products.forEach((currentProduct) => {
+        const card = document.createElement('div');
+        card.className = 'product-card';
+        card.innerHTML = `
+        <img src="${currentProduct.imgUrl}" alt="${currentProduct.name}">
+        <h3>${currentProduct.name}</h3>
+        <p class="rating-discount-strip"> ⭐${currentProduct.rating}
+            <span class="discount">${currentProduct.discountPercentage}% OFF </span>
+        </p>
+        <p class="price-button-strip">
+            <span class="price">${currentProduct.discountPrice}</span>
+            <span class="orignal-price">${currentProduct.price}</span>
+            <button id="cart-button">Add to Cart </button>
+        </p>
+        `;
+    productsDisplay.append(card)
+    });
+}
+displayProducts(products)
